7826a3716540568a78ebe64909d53e6f1b0f7057
BrunoMor9696 <bruno.mor9696@gmail.com>
2022-02-23 14:26:37 -0500

    fix: price change

--- react/components/AddChargesApp/actionsAdditionalApp.tsx
+++ react/components/AddChargesApp/actionsAdditionalApp.tsx
@@ -4,8 +4,8 @@ import { useProduct } from 'vtex.product-context'
 import { useOrderItems } from 'vtex.order-items/OrderItems'
 import { Wrapper } from 'vtex.add-to-cart-button'
 
-import { OrderForm } from 'vtex.order-manager'
-const { useOrderForm } = OrderForm
+// import { OrderForm } from 'vtex.order-manager'
+// const { useOrderForm } = OrderForm
 
 const CSS_HANDLES = [
   'buttonVisibility'
@@ -15,24 +15,28 @@ export const actionsAdditionalApp = () => {
 
   const { product, selectedItem } = useProduct()
   const { addItem } = useOrderItems()
-  const { orderForm } = useOrderForm()
+  // const { orderForm } = useOrderForm()
 
-  const productProperties = product?.properties
-  const shipAmmountQuantityObject = productProperties.filter(
-    (productProperty: any) => productProperty.name === 'AddShipAmtQty'
-  )
+  // const productProperties = product?.properties
+  // const shipAmmountQuantityObject = productProperties.filter(
+  //   (productProperty: any) => productProperty.name === 'AddShipAmtQty'
+  // )
 
-  const shipAmmountQuantity = parseInt(shipAmmountQuantityObject[0]?.values[0])
+  // const shipAmmountQuantity = parseInt(shipAmmountQuantityObject[0]?.values[0])
+
   const { skuSelector } = !!product && product
   const skuIsSelected = skuSelector?.areAllVariationsSelected
 
   const [valueAdditional, setValueAdditional] = useState(0)
   const [charges, setCharges] = useState(false)
-  const [quytProduct, setQytProduct] = useState(0)
+  // const [quytProduct, setQytProduct] = useState(0)
 
   const handles: any = useCssHandles(CSS_HANDLES)
 
   useEffect(() => {
+
+    console.log('productoo', product)
+
     setCharges(false)
     let dropdownContainer: any = document.getElementById("dropdownContainer")
 
@@ -61,40 +65,8 @@ export const actionsAdditionalApp = () => {
     }
   }, [product, valueAdditional, charges, selectedItem])
 
-  console.log('orderForm.items', orderForm.items);
-
   const addAdditionalCharges = () => {
-    let productIterations = Math.ceil(quytProduct / shipAmmountQuantity)
-    console.log("Deberé iterar", productIterations, "veces");
-    // for (let i = 0; i < quytProduct; i++) {
-    //   if ((i + 1) % shipAmmountQuantity === 0 || i === 0) {
-    //     console.log("Iteración con resto 0");
-    //     addItem([{
-    //       id: selectedItem.itemId,
-    //       quantity: 1,
-    //       seller: "1",
-    //       skuName: product.productName,
-    //       name: product.productName,
-    //       options: [
-    //         {
-    //           assemblyId: "Additional Charge_shipping",
-    //           id: 109139,
-    //           seller: "1",
-    //           quantity: 8 //Hard coded
-    //         }
-    //       ]
-    //     }])
-    //   } else {
-    //     console.log("Iteración con resto irregular");
-    //     addItem([{
-    //       id: selectedItem.itemId,
-    //       quantity: 1,
-    //       seller: "1",
-    //       skuName: product.productName,
-    //       name: product.productName
-    //     }])
-    //   }
-    // }
+    // let productIterations = Math.ceil(quytProduct / shipAmmountQuantity)
     addItem([{
       id: selectedItem.itemId,
       quantity: 10,
@@ -106,72 +78,83 @@ export const actionsAdditionalApp = () => {
           assemblyId: "Additional Charge_shipping",
           id: 109139,
           seller: "1",
-          quantity: 10 //Hard coded
+          quantity: 1
         }
       ]
     }])
   }
 
-  // useEffect(() => {
-  //   if (charges) {
-  //     const buttonAdd: any = document.querySelector(".vtex-minicart-2-x-minicartContainer .vtex-button")
-  //     const addCharges: any = document.getElementById("addCharges")
-  //     addCharges.addEventListener('click', function () {
-  //       console.log('buttonAdd', buttonAdd);
-  //     });
+  // setTimeout(() => {
+  //   const qytProduct: any = document.querySelector(`#quantity-dropdown-${selectedItem.itemId}`)?.textContent
+  //   const qytProductHigh: any = document.querySelector(`#quantity-input-${selectedItem.itemId}`)
+
+  //   if (qytProduct) {
+  //     setQytProduct(parseFloat(qytProduct))
   //   }
-  // }, [charges])
 
-  setTimeout(() => {
-    const qytProduct: any = document.querySelector(`#quantity-dropdown-${selectedItem.itemId}`)?.textContent
-    const qytProductHigh: any = document.querySelector(`#quantity-input-${selectedItem.itemId}`)
+  //   if (qytProductHigh) {
+  //     const qytProductHighNumber = qytProductHigh.value
+  //     setQytProduct(parseFloat(qytProductHighNumber))
+  //   }
 
-    if (qytProduct) {
-      setQytProduct(parseFloat(qytProduct))
-    }
+  // }, 0);
 
-    if (qytProductHigh) {
-      const qytProductHighNumber = qytProductHigh.value
-      setQytProduct(parseFloat(qytProductHighNumber))
-    }
+  // const options = {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //     Accept: 'application/json',
+  //     'X-VTEX-API-AppKey': 'vtexappkey-jefferspet-URQTXD',
+  //     'X-VTEX-API-AppToken': 'HMBFHPHQIKMCHGPBJVOFWUNHBHOSLETCNBOOBJYCXHVMETPTDHHBEANNZTEZAOFDRKOFSBZZWLHOYADHRVPEWRXNNAOHFZBICRVTFKABHIFSSZOYQXIFZBGZNARRMRUX'
+  //   },
+  //   body: JSON.stringify(
+  //     {
+  //       noSplitItem: true,
+  //       composition: {
+  //         items: [
+  //           {
+  //             id: "109139",
+  //             quantity: Math.ceil(quytProduct / 5) * 6,
+  //             seller: "1"
+  //           }
+  //         ]
+  //       }
+  //     }
+  //   ),
+  // };
 
-  }, 0);
-
-  console.log('cantidad a agregar', Math.ceil(quytProduct / 5) * 6);
+  // const addAdd = () => {
+  //   const fetching = () => {
+  //     fetch(`/api/checkout/pub/orderForm/3dd925b76f3742a6ab81e40065ea0455/items/0/assemblyOptions/Additional Charge_shipping?an=jefferspet`, options).then(
+  //       res => res.json()).then(data => {
+  //         console.log('data', data)
+  //       }).catch(err => console.error('error:' + err))
+  //   }
+  //   fetching()
+  // }
 
   const options = {
-    method: 'POST',
+    method: 'PUT',
     headers: {
-      'Content-Type': 'application/json',
       Accept: 'application/json',
+      'Content-Type': 'application/json',
       'X-VTEX-API-AppKey': 'vtexappkey-jefferspet-URQTXD',
       'X-VTEX-API-AppToken': 'HMBFHPHQIKMCHGPBJVOFWUNHBHOSLETCNBOOBJYCXHVMETPTDHHBEANNZTEZAOFDRKOFSBZZWLHOYADHRVPEWRXNNAOHFZBICRVTFKABHIFSSZOYQXIFZBGZNARRMRUX'
     },
-    body: JSON.stringify(
-      {
-        noSplitItem: true,
-        composition: {
-          items: [
-            {
-              id: "109139",
-              quantity: Math.ceil(quytProduct / 5) * 6,
-              seller: "1"
-            }
-          ]
-        }
-      }
-    ),
-  };
+    body: JSON.stringify({ price: 4000 })
+  }
 
-  const addAdd = () => {
+  const changePrice = () => {
     const fetching = () => {
-      fetch(`/api/checkout/pub/orderForm/3dd925b76f3742a6ab81e40065ea0455/items/0/assemblyOptions/Additional Charge_shipping?an=jefferspet`, options).then(
+      fetch(`/api/checkout/pub/orderForm/c03c671ab44e4721bf218e2ef47b0148/items/1/price`, options).then(
         res => res.json()).then(data => {
           console.log('data', data)
         }).catch(err => console.error('error:' + err))
     }
     fetching()
   }
+
+
 
   return (
     <>
@@ -202,7 +185,8 @@ export const actionsAdditionalApp = () => {
       } />
 
       {/* button fetch  */}
-      <button onClick={addAdd}>Add Additional</button>
+      {/* <button onClick={addAdd}>Add Additional</button> */}
+      <button onClick={changePrice}>ChangePrice</button>
     </>
   )
 }
