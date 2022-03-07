987778dca60d124fa406e6f8dbb72a26b577b36d
Esteban Monguí <esteband.itorres@gmail.com>
2022-03-07 16:33:36 -0500

    chore: add interface to 'AditionalCharges' component

--- react/components/AdditionalCharges/AdditionalCharges.tsx
+++ react/components/AdditionalCharges/AdditionalCharges.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { ReactElement } from 'react'
 
 interface Props{
-    children:any
+    children: ReactElement | ReactElement[]
 }
 
 const AdditionalCharges = ({ children }:Props) => {
