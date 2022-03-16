b6ef0a6a623f7deda0e1521b1d5153dfb02af2bf
Esteban Monguí <esteband.itorres@gmail.com>
2022-03-16 10:22:14 -0500

    chore: develop 'specificationsHelper' with methods to get, save and filter specifications

--- react/modules/specificationsHelper.ts
+++ react/modules/specificationsHelper.ts
@@ -1,30 +1,83 @@
-interface Props{
-    specificationId:string
+interface ContextProps{
+    productContext:string
+    specificationsName:string[]
+}
+interface GetterProps{
+    productId:string
+}
+interface Specification{
+    name:string
+}
+interface SpecificationData{
+    Name:string
 }
 
-interface saveProps{
-    specificationId:string
+interface GetSpecificationsProps extends ContextProps{
+    productId:string
+}
+interface SaveProps extends GetterProps{
     specifications:string
 }
 
-const specificationFieldName = 'specifications'
+const specificationFieldName = 'productSpecifications'
 
-export const getSpecification = ({ specificationId }:Props) =>{
-    if(!specificationId) return
+export const getLocalStorageSpecification = ({ productId }:GetterProps) =>{
+    if(!productId) return false
     
-    const specifications = localStorage.getItem(specificationFieldName) ?? ''
-    const specification = JSON.parse(specifications)[specificationId]
-    return specification
+    const specifications = localStorage.getItem(specificationFieldName) ?? '{}'
+    const productSpecification = JSON.parse(specifications)[productId]
+
+    return productSpecification && productSpecification
 }
 
-export const saveSpecification = ({ specificationId, specifications }:saveProps) => {
-    if(!specificationId) return
-    const currentSpecification = getSpecification({ specificationId })
+export const saveLocalStorageSpecification = ({ productId, specifications }:SaveProps) => {
+    if(!productId || !specifications) return
+    const currentSpecifications = getLocalStorageSpecification({ productId })
+    const updatedSpecifications = JSON.stringify({...currentSpecifications, [productId]:[...specifications]})
     localStorage.setItem(
         specificationFieldName, 
-        JSON.stringify({
-            ...currentSpecification, 
-            [specificationId]:specifications
-        })
+        updatedSpecifications
     )
+    return updatedSpecifications
+}
+
+export const getProductContextSpecifications = ({ productContext, specificationsName }:ContextProps) => {
+    const allSpecifications = JSON.parse(productContext)?.product?.properties
+    const match = (specification:Specification) => specificationsName?.includes(specification?.name)
+    const specifications = allSpecifications?.find?.(match)
+
+    return specifications ?? false
+}
+
+export const getProductSpecifications = ({specificationsName, productId}:any) => {
+    const additionalChargeItemId = '50114'
+    if(productId === additionalChargeItemId) return
+    let specifications
+    const url = `/api/catalog_system/pvt/products/${productId}/specification`
+
+    fetch(url)
+    .then(response=>response.json())
+    .then(data => {
+        const match = (specification:SpecificationData) => specificationsName?.includes(specification?.Name)
+        specifications = data?.filter(match)
+    })
+
+    return specifications
+}
+
+export const getSpecifications = ({ specificationsName, productContext, productId }:GetSpecificationsProps) =>{
+  
+  const specifications = ( 
+    getLocalStorageSpecification({ productId }) 
+    ? 
+    getLocalStorageSpecification({ productId }) 
+    :
+    getProductContextSpecifications({ productContext, specificationsName })
+    ?
+    getProductContextSpecifications({ productContext, specificationsName })
+    :
+    getProductSpecifications( productId )
+  )
+  
+  return specifications
 }
