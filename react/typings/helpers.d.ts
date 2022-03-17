d606ac457f5b7d3325010f35281a79c7b8271848
Esteban Monguí <esteband.itorres@gmail.com>
2022-03-16 23:30:20 -0500

    chore: createinterfaces to specifications helper

---
+++ react/typings/helpers.d.ts
@@ -0,0 +1,24 @@
+export interface ContextProps{
+    productContext:string
+    specificationsName:string[]
+}
+export interface GetterProps{
+    productId?:string
+}
+export interface Specification{
+    name:string
+    values:string[]
+}
+export interface SpecificationData{
+    Name:string
+}
+
+export interface GetProductSpecificationsProps extends GetterProps{
+    specificationsName:string[] | undefined
+}
+export interface GetSpecificationsProps extends ContextProps{
+    productId?:string
+}
+export interface SaveProps extends GetterProps{
+    specifications:any
+}
