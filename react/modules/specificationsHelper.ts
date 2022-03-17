bec08bdca4a477fddcf59a797fd15a3ce775cc5e
Esteban Monguí <esteband.itorres@gmail.com>
2022-03-16 23:25:19 -0500

    chore: develop 'specificationsHelper' to manage specifications methods

--- react/modules/specificationsHelper.ts
+++ react/modules/specificationsHelper.ts
@@ -1,69 +1,72 @@
-interface ContextProps{
-    productContext:string
-    specificationsName:string[]
-}
-interface GetterProps{
-    productId:string
-}
-interface Specification{
-    name:string
-}
-interface SpecificationData{
-    Name:string
-}
+import {
+    ContextProps, 
+    GetterProps, 
+    Specification,
+    GetSpecificationsProps,
+    SaveProps
+} from '@typings/helpers'
 
-interface GetSpecificationsProps extends ContextProps{
-    productId:string
-}
-interface SaveProps extends GetterProps{
-    specifications:string
-}
-
-const specificationFieldName = 'productSpecifications'
+const specificationFieldName = 'specifications'
 
 export const getLocalStorageSpecification = ({ productId }:GetterProps) =>{
-    if(!productId) return false
+    const specifications = localStorage.getItem(specificationFieldName)
     
-    const specifications = localStorage.getItem(specificationFieldName) ?? '{}'
-    const productSpecification = JSON.parse(specifications)[productId]
+    if (!specifications) return null
+    
+    const productSpecifications = JSON.parse(specifications)
 
-    return productSpecification && productSpecification
+    if(!!productId) return productSpecifications[productId]
+    
+    return productSpecifications && productSpecifications
 }
 
-export const saveLocalStorageSpecification = ({ productId, specifications }:SaveProps) => {
-    if(!productId || !specifications) return
-    const currentSpecifications = getLocalStorageSpecification({ productId })
-    const updatedSpecifications = JSON.stringify({...currentSpecifications, [productId]:[...specifications]})
+export const saveLocalStorageSpecifications = ({ productId, specifications }:SaveProps) => {
+    if(!productId || !specifications) throw new Error('Error at saveLocalStorageSpecifications: productId or specifications are undefined')
+    
+    const currentSpecifications = getLocalStorageSpecification({}) ?? {}
+    const parsedSpecifications = parseSpecifications(specifications)
+    
+    if(!parseSpecifications) return
+
+    const updatedSpecifications = JSON.stringify({...currentSpecifications, [productId]:parsedSpecifications})
     localStorage.setItem(
         specificationFieldName, 
         updatedSpecifications
     )
+
     return updatedSpecifications
 }
 
 export const getProductContextSpecifications = ({ productContext, specificationsName }:ContextProps) => {
-    const allSpecifications = JSON.parse(productContext)?.product?.properties
+    if(!productContext) return false
+    
+    const parsedProductContext = JSON.parse(productContext)
+    const productId  = parsedProductContext?.product?.productId
+    const allSpecifications = parsedProductContext?.product?.properties
     const match = (specification:Specification) => specificationsName?.includes(specification?.name)
-    const specifications = allSpecifications?.find?.(match)
+    const specifications = allSpecifications?.filter?.(match)
+
+    if(!!productId){
+        saveLocalStorageSpecifications({ productId, specifications })
+    }
 
     return specifications ?? false
 }
 
-export const getProductSpecifications = ({specificationsName, productId}:any) => {
-    const additionalChargeItemId = '50114'
-    if(productId === additionalChargeItemId) return
-    let specifications
-    const url = `/api/catalog_system/pvt/products/${productId}/specification`
-
-    fetch(url)
-    .then(response=>response.json())
-    .then(data => {
-        const match = (specification:SpecificationData) => specificationsName?.includes(specification?.Name)
-        specifications = data?.filter(match)
+export const parseSpecifications = (specifications:Specification[]) => {
+    let parsedSpecifications = {}
+    
+    specifications.map((specification:Specification) => {
+        const { name, values } = specification
+        parsedSpecifications = {
+            ...parsedSpecifications,
+            [name]:values.pop()
+        }
     })
 
-    return specifications
+    return parsedSpecifications
 }
+
 
 export const getSpecifications = ({ specificationsName, productContext, productId }:GetSpecificationsProps) =>{
   
@@ -76,7 +79,7 @@ export const getSpecifications = ({ specificationsName, productContext, productI
     ?
     getProductContextSpecifications({ productContext, specificationsName })
     :
-    getProductSpecifications( productId )
+    null
   )
   
   return specifications
