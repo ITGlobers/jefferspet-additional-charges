e3be58113a72b9811a36e9abd7d89771939bd241
Esteban Monguí <esteband.itorres@gmail.com>
2022-03-17 20:18:28 -0500

    chore: update useQuantityValidation to manage dinamic specifications and dinamic items

--- react/hooks/useQuantityValidation.tsx
+++ react/hooks/useQuantityValidation.tsx
@@ -1,45 +1,32 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { useOrderForm } from 'vtex.order-manager/OrderForm'
 import { useOrderItems } from 'vtex.order-items/OrderItems'
 import { quantityController } from '../helpers/quantityControls'
+import { Item } from '@typings/orderForm'
 
-interface Item{
-    productId:string
-    quantity:number
-    id:string
-}
-
-const selectedItemId = "25811"
 const additionalChargeId = "109139"
 
 const useQuantityValidation = () => {
   const [state, setState] = useState({
-    additionalQuantity:0,
-    selectedItemQuantity:0
+    additionalQuantity:0
   })
   const {orderForm:{ items }} = useOrderForm()
   const { addItem, removeItem, updateQuantity } = useOrderItems();
-  const selectedItem = items.find((item:Item) => item.productId === selectedItemId)
   const additionalsItem = items.find((item:Item) => item.id === additionalChargeId)
   const additionalsCurrentQuantity = additionalsItem?.quantity 
-  const addShipAmtQty = 6
-  const addShipAmt = 5
-  const selectedItemQuantity = selectedItem?.quantity
 
-  const includeAdditionalCharges = () => {
+  const includeAdditionalCharges = (quantityToAdd:number) => {
     const additionalChargeItem = {
       assemblyId: "Additional Charge_shipping",
       id: additionalChargeId,
       seller: "1",
-      quantity: addShipAmt,
+      quantity: quantityToAdd,
     }
     const itemToUpdate = JSON.stringify(additionalChargeItem)
     
     const quantities = {
-      currentQuantity:additionalsCurrentQuantity,
-      selectedItemQuantity, 
-      addShipAmtQty, 
-      addShipAmt
+      quantityToAdd, 
+      currentQuantity: additionalsCurrentQuantity
     }
     const methods = {
         addItem, 
@@ -48,19 +35,12 @@ const useQuantityValidation = () => {
     }
     
     const additionalQuantity = quantityController({methods, quantities, itemToUpdate})
-
+    
     setState({
       ...state,
       additionalQuantity
     })
-  }
-  
-  useEffect(()=>{
-    setState({
-      ...state,
-      selectedItemQuantity
-    })
-  }, [selectedItemQuantity])
+  }  
   
   return [includeAdditionalCharges, state]
 }
