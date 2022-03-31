0d6ec59a8e29e6b4ae167fd0cc5138ddd9ab5c4f
Esteban Monguí <esteband.itorres@gmail.com>
2022-03-31 16:13:46 -0500

    feat: create helper to add additional value at order placed summary

---
+++ react/helpers/addDomElements.ts
@@ -0,0 +1,21 @@
+import '../css/additionalsItem.css'
+
+interface Props{
+  classNode:string
+  additionalValue:string
+}
+
+export const addAdditionalValue = ({ classNode, additionalValue }:Props) => {
+  const baseNode = document.querySelector(classNode)
+  const additionalsItem = document.createElement('li')
+  additionalsItem.className = 'additionals-item pv3 flex justify-between items-center'
+  additionalsItem.innerHTML = `
+    <span class="c-on-base" style="color:rgb(85, 65, 58)">
+      Additional charges
+    </span>
+    <span class="c-on-base" style="color:rgb(85, 65, 58)">
+      ${ additionalValue }
+    </span>
+  `
+  baseNode?.after(additionalsItem)
+}
